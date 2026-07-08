549ccce08f598337f95a26f88e6d082ac1f33756
aowshad <alaowshad335@gmail.com>
2026-07-08 12:09:16 +0600

    Focus 1.2.0

--- Casks/focus.rb
+++ Casks/focus.rb
@@ -1,6 +1,6 @@
 cask "focus" do
-  version "1.1.0"
-  sha256 "47edb408f99b7743f5fb8adfbfa874b4ca6e0dfceb46ee5a6ece6393083b401b"
+  version "1.2.0"
+  sha256 "07274989e92acd8b9855f24dfaa945740e12ff0e2b2369c562cc24ad761bda67"
 
   url "https://github.com/aowshad/Focus/releases/download/v#{version}/Focus-#{version}-mac.zip"
   name "Focus"
